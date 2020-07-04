b61c706fd872b13d3d6521f48b7ab11cd1e7a6fe
prakhya24 <prakhyabolla@gmail.com>
2020-07-04 11:50:19 +0530

    Added code to accept horizontal input and also to play run animation and flip player according to horizontal input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,60 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator animator;
+    bool isRunning;
+
     private void Awake()
     {
         Debug.Log("Player COntroller is awake");
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //Debug.Log("collision: " + collision.gameObject.name);
+    //}
+    private void Update()
     {
-        Debug.Log("collision: " + collision.gameObject.name);
+        float speed = Input.GetAxisRaw("Horizontal");
+        // bool isRunning = true;
+
+         if(speed>0)
+        {
+            isRunning = true;
+            animator.SetBool("isRunning", true);
+            animator.SetFloat("Speed",speed);
+            Debug.Log("Player running right");
+            if (isRunning) flip();
+            //Vector3 scale = transform.localScale;
+
+            //scale.x = Mathf.Abs(scale.x);
+           // transform.localScale = scale;
+
+        }
+         else if (speed < 0)
+        {
+            isRunning = true;
+            animator.SetBool("isRunning", true);
+            animator.SetFloat("Speed", Mathf.Abs(speed));
+            Debug.Log("Player running left");
+            if (isRunning) flip();
+          
+        }
+     
+        void flip()
+        {
+            Vector3 scale = transform.localScale;
+            if (speed < 0)
+            {
+                scale.x = -1f * Mathf.Abs(scale.x);
+            }
+            else if (speed > 0)
+            {
+                scale.x = Mathf.Abs(scale.x);
+            }
+            transform.localScale = scale;
+        }
+
     }
 
 }
