3070a1410fbb3c0ed9931e21be92f68b21782f51
prakhya24 <prakhyabolla@gmail.com>
2020-07-05 12:24:18 +0530

    Added keyboard control to crouch animation (left control key) and also added vertical axis movement to jump animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,54 +5,91 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
-    bool isRunning;
+    bool isRunning,isJump;
 
     private void Awake()
     {
         Debug.Log("Player COntroller is awake");
     }
 
+    public BoxCollider2D PlayerCollider;
+    private float sizeX, sizeY;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        PlayerCollider = GetComponent<BoxCollider2D>();
+        sizeX = PlayerCollider.size.x;
+        sizeY = PlayerCollider.size.y;
+    }
+
+
     //private void OnCollisionEnter2D(Collision2D collision)
     //{
     //Debug.Log("collision: " + collision.gameObject.name);
     //}
+
     private void Update()
     {
         float speed = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
         // bool isRunning = true;
+     
 
-         if(speed>0)
+        //Running
+        if (speed != 0 && !isRunning)
         {
-            isRunning = true;
-            animator.SetBool("isRunning", true);
-            animator.SetFloat("Speed",speed);
-            Debug.Log("Player running right");
+            
+                animator.SetBool("isRunning", true);
+                animator.SetFloat("Speed", Mathf.Abs(speed));
+                isRunning = true;
+            
             if (isRunning) flip();
-            //Vector3 scale = transform.localScale;
-
-            //scale.x = Mathf.Abs(scale.x);
-           // transform.localScale = scale;
-
         }
-         else if (speed < 0)
+        else
         {
-            isRunning = true;
-            animator.SetBool("isRunning", true);
-            animator.SetFloat("Speed", Mathf.Abs(speed));
-            Debug.Log("Player running left");
-            if (isRunning) flip();
-          
+            isRunning = false;
+            animator.SetBool("isRunning", false);
+        }
+
+        //Jumping
+        if (vertical != 0 && !isJump)
+        {
+            
+                animator.SetBool("isJump", true);
+                isJump = true;
+            
+        }else
+        {
+            isJump = false;
+            animator.SetBool("isJump", false);
+        }
+
+        // Crouching
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+           // isCrouch = true;
+           // isRunning = false;
+            animator.SetBool("isCrouch", true);
+            PlayerCollider.size = new Vector2(x: sizeX, 2.24f);
+            Debug.Log("player is crouched");
+        }
+        else
+        {
+           // isCrouch = false;
+            //isRunning = true;
+            animator.SetBool("isCrouch", false);
+            PlayerCollider.size = new Vector2(x: sizeX, sizeY);
+            Debug.Log("player is not crouched");
         }
      
         void flip()
         {
             Vector3 scale = transform.localScale;
-            if (speed < 0)
-            {
+            if (speed < 0) { 
                 scale.x = -1f * Mathf.Abs(scale.x);
             }
-            else if (speed > 0)
-            {
+            else if (speed > 0){
                 scale.x = Mathf.Abs(scale.x);
             }
             transform.localScale = scale;
