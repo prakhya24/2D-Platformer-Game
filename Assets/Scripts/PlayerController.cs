80203dee95b89784ccf746774ceeb9950fbb1782
prakhya24 <prakhyabolla@gmail.com>
2020-07-06 16:38:24 +0530

    implemented changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Animator animator;
-    bool isRunning,isJump;
-
-    private void Awake()
-    {
-        Debug.Log("Player COntroller is awake");
-    }
-
+    private Animator animator;
+    bool isRunning, isJump;
     public BoxCollider2D PlayerCollider;
     private float sizeX, sizeY;
 
+
+    
     private void Start()
     {
         animator = GetComponent<Animator>();
@@ -23,78 +19,72 @@ public class PlayerController : MonoBehaviour
         sizeY = PlayerCollider.size.y;
     }
 
-
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //Debug.Log("collision: " + collision.gameObject.name);
-    //}
-
     private void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        // bool isRunning = true;
-     
+        
+        PlayerRun();
+        PlayerCrouch();
+        PlayerJump();
 
-        //Running
-        if (speed != 0 && !isRunning)
+    }
+    void PlayerRun()
+    {
+        float Horizontal = Input.GetAxisRaw("Horizontal");
+        
+        if (Horizontal != 0 && !isRunning)
         {
-            
-                animator.SetBool("isRunning", true);
-                animator.SetFloat("Speed", Mathf.Abs(speed));
-                isRunning = true;
-            
-            if (isRunning) flip();
+
+            animator.SetBool("isRunning", true);
+            animator.SetFloat("Speed", Mathf.Abs(Horizontal));
+            isRunning = true;
+
+            Vector3 scale = transform.localScale;
+            if (Horizontal < 0)
+            {
+                scale.x = -1f * Mathf.Abs(scale.x);
+            }
+            else if (Horizontal > 0)
+            {
+                scale.x = Mathf.Abs(scale.x);
+            }
+            transform.localScale = scale;
         }
         else
         {
             isRunning = false;
             animator.SetBool("isRunning", false);
         }
-
-        //Jumping
-        if (vertical != 0 && !isJump)
-        {
-            
-                animator.SetBool("isJump", true);
-                isJump = true;
-            
-        }else
-        {
-            isJump = false;
-            animator.SetBool("isJump", false);
-        }
-
-        // Crouching
+    }
+    void PlayerCrouch()
+    {
         if (Input.GetKey(KeyCode.LeftControl))
         {
-           // isCrouch = true;
-           // isRunning = false;
             animator.SetBool("isCrouch", true);
-            PlayerCollider.size = new Vector2(x: sizeX, 2.24f);
+            PlayerCollider.size = new Vector2(sizeX, sizeY);
             Debug.Log("player is crouched");
         }
         else
         {
-           // isCrouch = false;
-            //isRunning = true;
             animator.SetBool("isCrouch", false);
-            PlayerCollider.size = new Vector2(x: sizeX, sizeY);
+            PlayerCollider.size = new Vector2(sizeX, sizeY);
             Debug.Log("player is not crouched");
         }
-     
-        void flip()
-        {
-            Vector3 scale = transform.localScale;
-            if (speed < 0) { 
-                scale.x = -1f * Mathf.Abs(scale.x);
-            }
-            else if (speed > 0){
-                scale.x = Mathf.Abs(scale.x);
-            }
-            transform.localScale = scale;
-        }
-
     }
+    void PlayerJump()
+    {
+        float vertical = Input.GetAxisRaw("Vertical");
+        if (vertical != 0 && !isJump)
+        {
 
+            animator.SetBool("isJump", true);
+            isJump = true;
+
+        }
+        else
+        {
+            isJump = false;
+            animator.SetBool("isJump", false);
+        }
+    }
+    
 }
